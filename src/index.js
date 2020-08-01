c2df8417e376b5d93f59a6ee34977be3049940f2
Rishi <suryawanshirishi.nmims@gmail.com>
2020-08-01 17:59:41 +0530

    ReactJS4

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const EmployeeContext=React.createContext();
+class App extends React.Component {
+  constructor(props){
+    super(props); 
+    this.state = {
+      data: {Id:101},
+      changeEmployeeInfo:  this. updateEmployeeDetails,
+    };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  }
+  changeEmpData=()=>{
+        this.setState({data:{Id:102}});
+      }
+   render() {
+    return <div>
+      <h2>Welcome to App Component</h2>
+      <p>
+        <label>Employee Id : <b>{this.state.data.Id}</b></label>
+      </p>
+      <EmployeeContext.Provider value={this.state}>
+      <Employee />
+        </EmployeeContext.Provider> 
+        <p>
+         <button onClick={this. changeEmpData }>Change</button></p>
+        </div>
+  }
+}
+
+class Employee extends React.Component {
+  static contextType = EmployeeContext;
+  render() {
+    return <div>
+      <h2>Welcome to Employee Component...</h2>
+      <p>
+        <label>Employee Id : <b>{this.context.data.Id}</b></label>
+      </p>
+      <Salary></Salary>
+      </div>
+  }
+}
+
+class Salary extends React.Component{
+    static contextType=EmployeeContext;
+    render(){
+      return<div>
+        <h2>Welcome to Salary Component</h2>
+        <p><label>Employee Id: <b>{this.context.data.Id}</b></label></p>
+        
+      </div>
+    }
+  }
+
+const element=<App/>
+ReactDOM.render(element,document.getElementById('root'));
